2260ce4c6dfc289b729222bd66086f318a6a37a5
Artem Pr <yorgan.info@gmail.com>
2023-04-20 15:58:12 +0300

    Add project files.

---
+++ Git_Leson3_Ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Leson3_Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
